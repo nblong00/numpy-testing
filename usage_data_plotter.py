07774ac1302e46b627930aecc2d09826bff34b9a
Blaise <blazzze0517@gmail.com>
2025-08-19 13:08:02 -0400

    Separated logic into different functions. Added dunder main statement.

--- usage_data_plotter.py
+++ usage_data_plotter.py
@@ -1,13 +1,28 @@
 import numpy as np
 import csv
 
-with open("../../../pythontesting/usage-data.csv", mode="r") as f:
-    reader = csv.reader(f)
-    first_row = next(reader)
-    usage_data = np.array([
-        first_row
-    ], np.uint16)
-    print(usage_data.ndim)
-    for row in reader:
-        usage_data = np.append(usage_data, [row], axis=0)
-    print(usage_data)
+
+def gather_user_input():
+    user_input = input('Enter file relative or absolute path: ')
+    return user_input
+
+
+def array_generation(user_input):
+    with open(user_input, mode="r") as f:
+        reader = csv.reader(f)
+        first_row = next(reader)
+        usage_data = np.array([
+            first_row
+        ], np.uint16)
+        for row in reader:
+            usage_data = np.append(usage_data, [row], axis=0)
+        print(usage_data)
+
+
+def main():
+    user_input = gather_user_input()
+    array_generation(user_input)
+
+
+if __name__ == "__main__":
+    main()
