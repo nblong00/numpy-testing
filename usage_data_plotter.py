9540ed9045055036587615b9db67c41d9a848d87
Blaise <blazzze0517@gmail.com>
2025-08-24 21:44:26 -0400

    Added function to print mean weekly usage values to user. Also started squeezing the array to cut out un-needed dimension.

--- usage_data_plotter.py
+++ usage_data_plotter.py
@@ -8,18 +8,18 @@ def gather_user_input():
 
 
 def convert_array_values_to_int(row):
-    coverted_row = [int(i) for i in row]
-    return coverted_row
+    converted_row = [int(i) for i in row]
+    return converted_row
 
 
 def calc_mean(usage_data):
-    usage_mean = usage_data.mean(axis=2)
+    usage_mean = usage_data.mean(axis=1)
     return usage_mean
 
 
 def calc_min_max(usage_data):
-    calc_min = usage_data.min(axis=2)
-    calc_max = usage_data.max(axis=2)
+    calc_min = usage_data.min(axis=1)
+    calc_max = usage_data.max(axis=1)
     return calc_min, calc_max
 
 
@@ -33,7 +33,18 @@ def array_generation(user_input):
         for row in reader:
             converted_row = convert_array_values_to_int(row)
             usage_data = np.append(usage_data, [[converted_row]], axis=1)
+        usage_data = np.squeeze(usage_data)
         return usage_data
+
+
+def showcase_mean_results(usage_mean):
+    print("\nAverage daily usage over two week periods:\n")
+    for number, item in enumerate(usage_mean, 1):
+        if number != 1:
+            print(f"Week {number + 1} & {number + 2}: {np.round(item, 2)}")
+        else:
+            print(f"Week {number} & {number + 1}: {np.round(item, 2)}")
+    print("\n----------------------------------")
 
 
 def main():
@@ -41,9 +52,11 @@ def main():
     usage_data = array_generation(user_input)
     usage_mean = calc_mean(usage_data)
     (calc_min, calc_max) = calc_min_max(usage_data)
-    print(usage_mean)
-    print(calc_min)
-    print(calc_max)
+    showcase_mean_results(usage_mean)
+    # print(usage_data)
+    # print(usage_mean)
+    # print(calc_min)
+    # print(calc_max)
 
 
 if __name__ == "__main__":
